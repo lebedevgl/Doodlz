f9cf0cc84396af2d313e7d6a2b72cffcaf435788
Egor Lebedev <egor@abgcard.ru>
2016-12-02 20:49:41 +0300

    daily work

---
+++ app/src/main/java/com/deitel/doodlz/DoodleView.java
@@ -0,0 +1,19 @@
+package com.deitel.doodlz;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void saveImage() {
+
+    }
+
+    public void printImage() {
+
+    }
+}
--- app/src/main/java/com/deitel/doodlz/MainActivity.java
+++ app/src/main/java/com/deitel/doodlz/MainActivity.java
@@ -1,11 +1,10 @@
 package com.deitel.doodlz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        int screenSize =
+                getResources().getConfiguration().screenLayout &
+                        Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        else
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
 }
