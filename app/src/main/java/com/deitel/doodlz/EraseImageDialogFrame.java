8df68309585ee6693aeb0a07d43e986246d04ab4
Egor Lebedev <egor@abgcard.ru>
2016-12-05 16:35:45 +0300

    complete EraseImageDialog development

--- app/src/main/java/com/deitel/doodlz/EraseImageDialogFrame.java
+++ app/src/main/java/com/deitel/doodlz/EraseImageDialogFrame.java
@@ -1,6 +1,49 @@
 package com.deitel.doodlz;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
 public class EraseImageDialogFrame extends DialogFragment{
+    @Override
+    public Dialog onCreateDialog(Bundle bundle) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.message_erase);
+
+        builder.setPositiveButton(R.string.button_set_line_width,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getDoodleFragment().getDoodleView().clear();
+                    }
+                }
+        );
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create();
+    }
+
+    private MainActivityFragment getDoodleFragment() {
+        return (MainActivityFragment) getFragmentManager().findFragmentById(R.id.doodleView);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        MainActivityFragment fragment = getDoodleFragment();
+        if (fragment != null)
+            fragment.setDialogOnScreen(true);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        MainActivityFragment fragment = getDoodleFragment();
+        if (fragment != null)
+            fragment.setDialogOnScreen(false);
+    }
+
 }
